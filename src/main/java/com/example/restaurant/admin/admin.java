8a8bd87c4a62db6acc9251610a05b14352e4447c
HariomGeete <hariomgeete2@gmail.com>
2025-08-17 21:25:55 +0530

    add Admin service

---
+++ src/main/java/com/example/restaurant/admin/admin.java
@@ -0,0 +1,19 @@
+package com.example.restaurant.admin;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Table(name = "admin")
+@Data
+public class admin {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    int id;
+    String aemail;
+    String apassword;
+}
---
+++ src/main/java/com/example/restaurant/admin/adminRepo.java
@@ -0,0 +1,7 @@
+package com.example.restaurant.admin;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface adminRepo extends JpaRepository<admin,Integer>{
+    
+}
